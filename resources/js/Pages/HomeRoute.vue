2466c0a4f14f4af55a7f7d3a98ed4531f66c68d0
Ricardo Segundo Ferreira <gruporsf@gmail.com>
2025-07-07 14:20:42 -0300

    Configurado para trabalhar com vue

---
+++ resources/js/Pages/HomeRoute.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+import axios from "axios";
+import { ref } from "vue";
+
+const response = ref();
+
+const getValue = async () => {
+    try {
+        response.value = await axios.get("/api/test-me");
+    } catch (error) {
+        // Do something with the error
+        console.log(error);
+    }
+};
+
+</script>
+
+<template>
+    <div>
+        <h2>HOME</h2>
+        <p>
+            <router-link to="/test"> Take me to Test page </router-link>
+        </p>
+        <p>
+            <button type="button" @click.prevent="getValue()">Trigger Endpoint</button>
+            <p v-if="response">{{ response.data }}</p>
+        </p>
+    </div>
+</template>
